8183f2f89eb1b1cb6ce857037900747939392d27
xBizzardd <nathanbako24@gmail.com>
2026-09-20 02:10:48 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- bot.py
+++ bot.py
@@ -3,6 +3,20 @@ from discord.ext import commands
 from discord import app_commands
 import random
 import os
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevCodeBot działa!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web, daemon=True).start()
 
 
 # ============================================================
